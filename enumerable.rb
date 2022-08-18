6aadc37494c6ce5719a27f05f7227f5d9271bde6
Gonzalo <mgmediaweb@gmail.com>
2022-08-18 16:03:59 -0700

    testing the system

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,25 @@
 module MyEnumerable
   def all_method
-    list.each do |e|
-      return false unless yield e
+    list.each do |num|
+      return false unless yield num
     end
     true
   end
 
   def any_method
-    list.each do |e|
-      return true if yield e
+    list.each do |num|
+      return true if yield num
     end
     false
   end
 
   def filter_method
     result = []
-    list.each do |e|
-      result << e if yield e
+    list.each do |num|
+      result << num if yield num
     end
     result
   end
 end
+
+# WE NEED TESTING THIS
